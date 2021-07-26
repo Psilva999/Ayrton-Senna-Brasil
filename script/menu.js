b561e2d8952a290d0aca700b70a6301ce8b281a4
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-07-26 12:44:42 -0300

    Testando a responsividade

---
+++ script/menu.js
@@ -0,0 +1,47 @@
+
+// Animação do navbar
+class navbar {
+   constructor(barras, listas, ligados) {
+      this.barras = document.querySelector(barras)
+      this.listas = document.querySelector(listas)
+
+      this.ligados = document.querySelectorAll(ligados)
+      this.activeClass = 'active'
+
+      this.handleClick = this.handleClick.bind(this)
+   }
+
+   animados() {
+      this.ligados.forEach((link, index) => {
+         link.style.animation
+            ? (link.style.animation = '')
+            : (link.style.animation = `navLinkFade 0.7s ease forwards ${index / 7 + 0.5}s`)
+      })
+   }
+
+   handleClick() {
+      this.listas.classList.toggle(this.activeClass)
+      this.barras.classList.toggle(this.activeClass)
+
+      this.animados()
+   }
+
+   addClickEvent() {
+      this.barras.addEventListener('click', this.handleClick)
+   }
+
+   init() {
+      if (this.barras) {
+         this.addClickEvent()
+      }
+
+      return this
+   }
+}
+
+const responsivo = new navbar(
+   '.navbar',
+   '.lista',
+   '.lista li')
+
+responsivo.init()
