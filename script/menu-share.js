b48c6a2651abcbbe7e495fd08b24ededb900500a
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-08-13 06:35:19 -0300

    Fiz o menu abrir uma div que escurece a tela

---
+++ script/menu-share.js
@@ -0,0 +1,145 @@
+var evento = {
+   menu: document.querySelector('#simples-menu'),
+   burguer: document.querySelector('#btn-mobile'),
+   fechaMenuPorFora: document.querySelector('.fecha-menu')
+}
+
+evento.menu.addEventListener('click', () => {
+
+   evento.menu.classList.add('active')
+   evento.fechaMenuPorFora.style.display = 'block'
+})
+
+
+
+// function toggleMenu(event) {
+//    if (event.type === 'touchstart') event.preventDefault()
+//    mexeNo.burguer.classList.toggle('active')
+
+//    const active = mexeNo.burguer.classList.contains('active')
+//    event.currentTarget.setAttribute('aria-expanded', active)
+
+//    if (active) {
+//       event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+//    }
+
+//    else {
+//       event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+//    }
+// }
+// mexeNo.burguer.addEventListener('click', toggleMenu)
+// mexeNo.burguer.addEventListener('touchstart', toggleMenu)
+
+// var compartilha = document.querySelector('.share')
+
+// mexeNo.burguer.addEventListener('click', () => {
+//    if (mexeNo.burguer.classList.contains('active')) {
+
+//       mexeNo.clicaFora.style.display = 'block'
+//       mexeNo.lista.style.display = 'flex'
+
+//       compartilha.style.display = 'none'
+//    }
+
+//    else {
+//       mexeNo.clicaFora.style.display = 'none'
+//       mexeNo.lista.style.display = 'none'
+
+//       compartilha.style.display = 'flex'
+//       mexeNo.imagemOriginal.src = 'media/copy.svg'
+//    }
+// })
+
+// mexeNo.clicaFora.addEventListener('click', () => {
+//    mexeNo.lista.style.display = 'none'
+//    mexeNo.clicaFora.style.display = 'none'
+
+//    mexeNo.burguer.classList.remove('active')
+//    compartilha.style.display = 'flex'
+//    mexeNo.imagemOriginal.src = 'media/copy.svg'
+// })
+
+// var copiarFrase = document.querySelector('.aprendendo')
+
+// copiarFrase.addEventListener('click', function (event) {
+//    var copiaMuda = {
+//       frase: document.querySelector('#fraseCopiada'),
+//       imagem: document.querySelector('#imagemIndicando')
+//    }
+
+//    event.preventDefault()
+
+//    copiaMuda.frase.select()
+//    copiaMuda.frase.setSelectionRange(0, 9999)
+//    document.execCommand("copy") //Não encontrei um substituto
+
+//    copiaMuda.imagem.src = 'media/check.svg'
+
+//    var time = 1 * 60
+//    setInterval(apagarIndicadorDeCopy, 1000)
+
+//    function apagarIndicadorDeCopy() {
+//       var segundos = time % 60
+
+//       time--
+//       if (segundos >= 59) {
+//          copiaMuda.imagem.src = 'media/copy.svg'
+//       }
+
+//    }
+// })
+
+// var mexeNo = {
+//    compartilha: document.querySelector('.share'),
+//    container: document.querySelector('.container'),
+
+//    copiarLink: document.querySelector('.copiarLink'),
+//    linkDoSite: document.querySelector('#link'),
+//    indicaQueCopiou: document.querySelector('#imagem'),
+
+//    fechaPorFora: document.querySelector('.fechaContainer'),
+//    fechaLinks: document.querySelector('.fecha')
+// }
+
+// mexeNo.compartilha.addEventListener('click', () => {
+//    mexeNo.container.style.display = 'block'
+
+//    mexeNo.fechaPorFora.style.display = 'block'
+// })
+
+// mexeNo.fechaLinks.addEventListener('click', () => {
+//    mexeNo.container.style.display = 'none'
+
+//    mexeNo.fechaPorFora.style.display = 'none'
+// })
+
+// mexeNo.fechaPorFora.addEventListener('click', () => {
+//    mexeNo.container.style.display = 'none'
+//    mexeNo.fechaPorFora.style.display = 'none'
+
+//    mexeNo.indicaQueCopiou.src = 'media/copy.svg'
+// })
+
+// mexeNo.copiarLink.addEventListener('click', () => {
+
+//    event.preventDefault()
+//    mexeNo.linkDoSite.select()
+
+//    mexeNo.linkDoSite.setSelectionRange(0, 9999)
+//    document.execCommand("copy") //Não encontrei um substituto
+
+//    mexeNo.indicaQueCopiou.src = 'media/check.svg'
+
+//    var time = 1 * 60
+//    setInterval(apagarIndicadorDeCopy, 1000)
+
+//    function apagarIndicadorDeCopy() {
+//       var segundos = time % 60
+
+//       time--
+//       if (segundos >= 59) {
+//          mexeNo.indicaQueCopiou.src = 'media/copy.svg'
+//       }
+
+//    }
+// })
--- script/player.js
+++ script/player.js
@@ -78,3 +78,55 @@ var i = setInterval(function() {
       clearInterval(i)
    }
 }, 200)
+
+var ligaDesligaLegenda = document.querySelector('.legenda')
+
+ligaDesligaLegenda.addEventListener('click', () => {
+   var subtitle = document.querySelector('.conteudo-legenda')
+
+   //Retira a legenda
+   if (subtitle.classList.contains('active')) {
+      ligaDesligaLegenda.style.borderBottom = '1px solid black'
+
+      subtitle.classList.remove('active')
+      subtitle.style.display = 'none'
+   }
+
+   //Ativa a legenda
+   else {
+      ligaDesligaLegenda.style.borderBottom = '3px solid black'
+
+      subtitle.classList.add('active')
+      subtitle.style.display = 'flex'
+   }
+})
+
+//Momento em que a legenda aparece
+var video = document.querySelector('#video')
+
+video.addEventListener('timeupdate', function () {
+   var tempoEm = {
+      minutos: Math.floor(video.currentTime / 60, 10),
+      segundos: Math.floor(video.currentTime % 60)
+   }
+
+   var alteraSubtitle = document.querySelector('.conteudo-legenda')
+
+   if (tempoEm.segundos > 20 && tempoEm.segundos <= 21 && tempoEm.minutos == 0) {
+      alteraSubtitle.style.background = '#F2F2F2'
+
+      alteraSubtitle.innerHTML = `Eu corro destinado a vencer`
+   }
+
+   else if (tempoEm.segundos >= 24 && tempoEm.segundos <= 27) {
+      alteraSubtitle.style.background = ''
+
+      alteraSubtitle.innerHTML = ''
+   }
+
+   else if (tempoEm.segundos >= 30) {
+      alteraSubtitle.style.background = '#F2F2F2'
+
+      alteraSubtitle.innerHTML = `Eu continuo dando o meu melhor`
+   }
+})
